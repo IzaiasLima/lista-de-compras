fc608e9ed9e2db03122acfe2b8fee2853cc8dc40
Izaias Moreira Lima <izaiasmlima@gmail.com>
2026-02-19 23:32:59 -0300

    Ajustar import de lib

--- db.py
+++ db.py
@@ -1,13 +1,12 @@
 import os
 import sqlite3
-import psycopg2
+import psycopg2 as psql
 from psycopg2.extras import DictCursor
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
-psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
-psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
 
 TYPE_SQLITE = "sqlite"
 TYPE_MYSQL = "msql"
@@ -28,7 +27,7 @@ class DB(object):
 
         if not hasattr(self, "con"):
             if DB_TYPE == TYPE_PSQL:
-                self.con = psycopg2.connect(DATABASE_URL)
+                self.con = psql.connect(DATABASE_URL)
                 self.cur = self.con.cursor(cursor_factory=DictCursor)
 
             elif DB_TYPE == TYPE_SQLITE:
@@ -36,15 +35,13 @@ class DB(object):
                 self.con.row_factory = sqlite3.Row
                 self.cur = self.con.cursor()
 
-            
-
             elif DB_TYPE == TYPE_MYSQL:
                 raise NotImplementedError("Conexão com o MySql não implementada.")
 
             else:
                 raise NotImplementedError("Tipo de conexão não implementada.")
-            
-            print (f"Sistema conectado à base de dados: {DATABASE_URL}.")
+
+            print(f"Sistema conectado à base de dados: {DATABASE_URL}.")
 
 
 if __name__ == "__main__":
